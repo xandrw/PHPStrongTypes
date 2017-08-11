8f8c87fdcea971705c1a3a19707dd8597a091e10
xandrw <alex.cosor@request.ro>
2017-08-11 18:39:59 +0300

    Added type aliasing
    Refactored getAliasType
    
    
    Simplified makeTypeFromClassName
    
    
    Fixed test

--- Type.php
+++ Type.php
@@ -4,7 +4,8 @@ abstract class Type
 {
     private $value;
     private $type = null;
-    private $types = ['boolean', 'integer', 'double', 'string', 'array', 'object'];
+    private static $types = ['boolean', 'integer', 'double', 'string', 'array', 'object'];
+    private static $aliases = [];
     
     final public function __construct($value)
     {
@@ -14,30 +15,20 @@ abstract class Type
         $this->value = $value;
     }
     
-    final private function makeTypeFromClassName()
-    {
-        $className = strtolower(static::class);
-        $className = preg_replace('/(type)+$/', '', $className);
-
-        $this->type = $className;
-    }
-    
-    final private function checkType()
-    {
-        if (! in_array($this->type, $this->types) || $this->type === null)
-            throw new InvalidArgumentException("Invalid type given `{$this->type}`.");
-    }
-    
-    final private function checkValueType($value)
-    {
-        $type = gettype($value);
-        if ($type !== $this->type)
-            throw new InvalidArgumentException("Value must be `{$this->type}`, `{$type}` given.");
-    }
-    
     final public static function make($value)
     {
+        if (static::class === self::class)
+            throw new InvalidArgumentException("Cannot call make on the abstract Type.");
+        
         return new static($value);
+    }
+    
+    final public static function alias($alias, $type)
+    {
+        $alias = strtolower($alias);
+        self::checkAliasType($type);
+        self::checkDuplicateAliasType($alias);
+        self::$aliases[$type][] = $alias;
     }
     
     final public function __invoke()
@@ -51,5 +42,46 @@ abstract class Type
             return serialize($this->value);
         
         return (string) $this->value;
+    }
+    
+    final private function makeTypeFromClassName()
+    {
+        $className = strtolower(static::class);
+        $className = preg_replace('/(type)+$/', '', $className);
+        $this->type = self::getAliasType($className);
+    }
+    
+    final private static function getAliasType($alias)
+    {
+        foreach (self::$aliases as $type => $aliases)
+            if (is_array($aliases) && in_array($alias, $aliases)) return $type;
+        return $alias;
+    }
+    
+    final private function checkType()
+    {
+        if (! in_array($this->type, self::$types) || $this->type === null)
+            throw new InvalidArgumentException("Invalid type given `{$this->type}`.");
+    }
+    
+    final private function checkValueType($value)
+    {
+        $type = gettype($value);
+        if ($type !== $this->type)
+            throw new InvalidArgumentException("Value must be `{$this->type}`, `{$type}` given.");
+    }
+    
+    final private static function checkAliasType($type)
+    {
+        if (! in_array($type, self::$types))
+            throw new InvalidArgumentException("Invalid type given `{$type}`.");
+    }
+    
+    final private static function checkDuplicateAliasType($alias)
+    {
+        foreach (self::$aliases as $type => $aliases) {
+            if (is_array($aliases) && in_array($alias, $aliases))
+                throw new InvalidArgumentException("The {$alias} alias is already set.");
+        }
     }
 }
